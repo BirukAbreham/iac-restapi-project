c8d38eb22c8454b33cc0c8b0066abbe0732953cd
BirukAbreham <birukabreham0@gmail.com>
2021-05-07 21:19:53 +0000

    will added CORS origin access on main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Cors enable
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // Start listening on port 3000
   await app.listen(3000);
 }
 bootstrap();
